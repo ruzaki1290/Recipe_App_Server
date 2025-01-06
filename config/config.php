3c887fcb86ab53e32674149afa296f69dca10182
Rus <rus.zakirov@triosstudent.com>
2025-01-06 09:09:45 +0300

    Made some modifications to the code to fix the error

--- config/config.php
+++ config/config.php
@@ -13,13 +13,12 @@ if (in_array($origin, $allowedOrigins)) {
 
 header('Access-Control-Allow-Headers: ' . implode(', ', $allowedMethods));
 
-if (isset('HTTP_ACCESS_CONTROL_REQUEST_HEADERS')) {
-   $requestHeaders = explode(',', $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']);
+if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+   $requestHeaders = explode(',', $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'] ?? '');
    $requestHeaders = array_map(trim, $requestHeaders); // Trim whitespace from headers
    if (count(array_intersect($requestHeaders, $allowedHeaders)) == count($requestHeaders)) {
       header('Access-Control-Allow-Headers: ' . implode(', ', $requestHeaders));
    }
 }
-// header('Access-Control-Allow-Methods: ' . implode(',', $allowedMethods)
 
 ?>
